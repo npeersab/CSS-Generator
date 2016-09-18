46963b8bddfb96ec092907a3c44555f6165b6e19
npeersab <npeersab77@gmail.com>
2016-09-18 23:30:09 +0530

    fixed some minor bugs

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
