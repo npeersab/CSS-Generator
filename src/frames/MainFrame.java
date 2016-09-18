7b034e9d972d2a67b6849fd891201a3979bf4486
npeersab <npeersab77@gmail.com>
2016-09-18 18:40:18 +0530

    added file chooser

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -1,20 +1,24 @@
 package frames;
 
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-
 import javax.swing.JPanel;
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import css.CSSFile;
 
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	JPanel buttonPanel; 
-	JButton newfileButton, openfileButton;
+	private JPanel buttonPanel; 
+	private JButton newfileButton, openfileButton;
+	private CSSFile cssfile;
 	
 	public MainFrame() throws IOException {
 		
@@ -25,9 +29,12 @@ public class MainFrame extends JFrame {
 		newfileButton = new JButton("New");
 		newfileButton.setBounds(770, 520, 100, 30);
 		buttonPanel.add(newfileButton);
+		
 		openfileButton = new JButton("Open");
 		openfileButton.setBounds(890, 520, 100, 30);
 		buttonPanel.add(openfileButton);
+		OpenFile open = new OpenFile();
+		openfileButton.addActionListener(open);
 		
 		add(buttonPanel);
 		setTitle("CSS Generator");
@@ -36,5 +43,20 @@ public class MainFrame extends JFrame {
 		setIconImage(ImageIO.read(new File("res/icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	public class OpenFile implements ActionListener {
+		
+		public void actionPerformed(ActionEvent e) {
+			
+			String home = System.getProperty("user.home");
+			File cssdir = new File(home, "css_generator");
+			JFileChooser filechooser = new JFileChooser(cssdir);
+			filechooser.setDialogTitle("Select File");
+			filechooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			filechooser.showOpenDialog(new JLabel());
+			cssfile = new CSSFile(filechooser.getSelectedFile()); 
+								
+		}
 	}
 }
