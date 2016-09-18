4d792c903fe4f37de2a43ab7e4cd440747642448
npeersab <npeersab77@gmail.com>
2016-09-18 14:01:14 +0530

    added icon

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -3,20 +3,24 @@ package frames;
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JPanel;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 
 public class MainFrame extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	JPanel buttonPanel; 
 	JButton newfileButton, openfileButton;
 	
-	public MainFrame() {
+	public MainFrame() throws IOException {
 		
 		setLayout(new BorderLayout());
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(null);
-		buttonPanel.setAlignmentX(500);
 		
 		newfileButton = new JButton("New");
 		newfileButton.setBounds(770, 520, 100, 30);
@@ -29,6 +33,7 @@ public class MainFrame extends JFrame {
 		setTitle("CSS Generator");
 		setSize(1020, 600);
 		setLocation(150, 100);
+		setIconImage(ImageIO.read(new File("res/icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
--- src/main/mainclass.java
+++ src/main/mainclass.java
@@ -1,6 +1,5 @@
 package main;
 
-import css.CSSFile;
 import frames.MainFrame;
 
 public class mainclass {
