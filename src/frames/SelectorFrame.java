c07e0437540c01973f85cc4d5d0b2931c334ccef
npeersab <npeersab77@gmail.com>
2016-11-15 20:23:19 +0530

    updated selector frame

--- src/frames/SelectorFrame.java
+++ src/frames/SelectorFrame.java
@@ -4,9 +4,11 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class SelectorFrame extends JFrame {
 
@@ -24,13 +26,30 @@ public class SelectorFrame extends JFrame {
 		
 		add(new JLabel("Select Selector Type : "), bagConstraints);
 		
+		bagConstraints.gridy++;
+		add(new JLabel("Enter Selector name : "), bagConstraints);
+		
+		JButton cancelButton = new JButton("Cancel");
+		bagConstraints.gridy++;
+		add(cancelButton, bagConstraints);
+		
 		String selectors[] = {"Universal", "Element Type Selector", "ID Selector", "Class Selector"};
 		JComboBox<String> selector = new JComboBox<String>(selectors);
 		bagConstraints.gridx++;
+		bagConstraints.gridy = 0;
 		add(selector, bagConstraints);
 		
+		JTextField textField = new JTextField();
+		bagConstraints.gridy++;
+		add(textField, bagConstraints);
+		
+		bagConstraints.insets = new Insets(10, 20, 10, 0);
+		JButton addButton = new JButton("Add Selector");
+		bagConstraints.gridy++;
+		add(addButton, bagConstraints);
+					
 		setIconImage(ImgSrc.getImageIcon());
-		setSize(500, 200);
+		setSize(400, 200);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setVisible(true);
