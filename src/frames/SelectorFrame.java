5bdc4da6b8732bbb2a7c159374f01f60567eba2a
npeersab <npeersab77@gmail.com>
2016-12-04 20:16:54 +0530

    fixed problem with parent focusing, separeted listener and added comments

--- src/frames/SelectorFrame.java
+++ src/frames/SelectorFrame.java
@@ -3,9 +3,13 @@ package frames;
 import css.Selector;
 import css.SelectorType;
 import res.ImgSrc;
+
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JButton;
@@ -15,7 +19,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class SelectorFrame extends JFrame {
+public class SelectorFrame extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private JComboBox<SelectorType> selector;
@@ -25,18 +29,20 @@ public class SelectorFrame extends JFrame {
 
 		this.parent = parent;
 		enableParent(false);
+		
+		// set layout and GridBagConstraints
 		setLayout(new GridBagLayout());
 		GridBagConstraints bagConstraints = new GridBagConstraints();
-		
 		bagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		bagConstraints.gridx = bagConstraints.gridy = 0;
 		bagConstraints.insets = new Insets(10, 5, 10, 0);
 		
+		// add labels
 		add(new JLabel("Select Selector Type : "), bagConstraints);
-		
 		bagConstraints.gridy++;
 		add(new JLabel("Enter Selector name : "), bagConstraints);
 		
+		// add cancel button
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(e -> {
 			dispose();
@@ -46,7 +52,7 @@ public class SelectorFrame extends JFrame {
 		bagConstraints.gridy++;
 		add(cancelButton, bagConstraints);
 		
-		
+		// add selector type comboBox
 		SelectorType selectorType[] = SelectorType.values();
 		selector = new JComboBox<SelectorType>(selectorType);
 		selector.addActionListener( e -> {
@@ -64,42 +70,16 @@ public class SelectorFrame extends JFrame {
 		bagConstraints.gridy = 0;
 		add(selector, bagConstraints);
 		
+		// add textField to get selector name
 		textField = new JTextField();
+		textField.setFont(new Font("Arial", Font.PLAIN, 15));
 		bagConstraints.gridy++;
 		add(textField, bagConstraints);
 		
+		//  add addButton
 		bagConstraints.fill = GridBagConstraints.NONE;
 		JButton addButton = new JButton("Add Selector");
-		addButton.addActionListener(e -> {
-			String selectorName = textField.getText();
-			if(selectorName.length() == 0) {
-				JOptionPane.showMessageDialog(SelectorFrame.this, 
-						"Please enter Selector name", "No Selector name", 
-						JOptionPane.ERROR_MESSAGE);
-			}
-			else {
-				SelectorType type = (SelectorType) selector.getSelectedItem();
-				
-				switch (type) {
-				case class_selector:
-					selectorName = "." + selectorName;
-					break;
-				case id_selector:
-					selectorName = "#" + selectorName;
-					break;
-				case element_type_selector:
-					break;
-				case universal_selector:
-					break;
-				default:
-					break;				
-				}
-				Selector temp_selector = new Selector(selectorName, type);
-				parent.addSelector(temp_selector);
-				dispose();
-				enableParent(true);
-			}
-		});
+		addButton.addActionListener(this);
 		bagConstraints.gridy++;
 		add(addButton, bagConstraints);
 					
@@ -111,14 +91,15 @@ public class SelectorFrame extends JFrame {
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				dispose();
 				enableParent(true);
+				dispose();
 			}
 		});
 		setVisible(true);
 		setAlwaysOnTop(true);
 	}
 	
+	// enable or disable MainFrame
 	public void enableParent(boolean b) {
 		parent.setFocusableWindowState(b);
 		parent.setEnabled(b);
@@ -126,5 +107,38 @@ public class SelectorFrame extends JFrame {
 		if (b)
 			parent.addWindowListener(parent.getWindowAdapter());
 		parent.revalidate();	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String selectorName = textField.getText();
+		// display error if no name entered
+		if(selectorName.length() == 0) {
+			JOptionPane.showMessageDialog(SelectorFrame.this, 
+					"Please enter Selector name", "No Selector name", 
+					JOptionPane.ERROR_MESSAGE);
+		}
+		else {
+			SelectorType type = (SelectorType) selector.getSelectedItem();
+			// set symbol to selector according to type
+			switch (type) {
+			case class_selector:
+				selectorName = "." + selectorName;
+				break;
+			case id_selector:
+				selectorName = "#" + selectorName;
+				break;
+			case element_type_selector:
+				break;
+			case universal_selector:
+				break;
+			default:
+				break;				
+			}
+			Selector temp_selector = new Selector(selectorName, type);
+			parent.addSelector(temp_selector);
+			dispose();
+			enableParent(true);
+		}		
 	}
 }
