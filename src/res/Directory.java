fbeb56a9276e6fb5352e3a01d9e9cc9e737b8d8f
npeersab <npeersab77@gmail.com>
2016-12-04 21:09:58 +0530

    added comments

--- src/res/Directory.java
+++ src/res/Directory.java
@@ -3,9 +3,12 @@ package res;
 import java.io.File;
 
 public class Directory {
+	
+	// return CSS home directory
 	public static File getCSSDirectory() {
 		String home = System.getProperty("user.home");
 		File file = new File(home, "CSS Generator");
+		// if directory is not present create new
 		if(!file.exists())
 			file.mkdir();
 		return file;
