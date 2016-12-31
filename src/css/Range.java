987c8f5218678c9b2ad51980c491d6ef7642550f
npeersab <npeersab77@gmail.com>
2016-12-31 21:31:24 +0530

    added comments :P

--- src/css/Range.java
+++ src/css/Range.java
@@ -3,15 +3,18 @@ package css;
 public class Range<type> {
 	private type min, max;
 	
+	// constructor
 	public Range(type min, type max) {
 		this.min = min;
 		this.max = max;
 	}
 	
+	// return minimum value
 	public type getMin() {
 		return min;
 	}
 	
+	// return maximum value
 	public type getMax() {
 		return max;
 	}
--- src/main/dialog/ConfimationEventListener.java
+++
@@ -1,5 +0,0 @@
-package main.dialog;
-
-public interface ConfimationEventListener {
-	public void handleConfirmationEvent(ConfirmationEvent event);
-}
