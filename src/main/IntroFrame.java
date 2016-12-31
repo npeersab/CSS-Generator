27eb592226914ab0bbe862db1d5137193a61391a
npeersab <npeersab77@gmail.com>
2016-12-31 21:32:23 +0530

    converted to dialog

--- src/main/IntroFrame.java
+++ src/main/IntroFrame.java
@@ -1,25 +1,21 @@
 package main;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import res.ImgSrc;
 
-public class IntroFrame extends JFrame {
+public class IntroFrame extends JDialog {
 	private static final long serialVersionUID = 1L;
 
 	public IntroFrame() {
-
 		// set image
 		add(new JLabel(new ImageIcon(ImgSrc.getIntroImage())));
 		setSize(600, 236);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setUndecorated (true);
+		setUndecorated(true);
 		setLocationRelativeTo(null);
 		setAlwaysOnTop(true);
 		setResizable(false);
-		setIconImage(ImgSrc.getImageIcon());
-		setTitle("Starting CSS Generator");
 		setVisible(true);
 	}
 }
